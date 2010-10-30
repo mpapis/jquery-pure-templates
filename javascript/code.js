4fed7cfdfdcfa3c811c3920c20582c65f13e160f
Michal Papis <mpapis@niczsoft.com>
2010-10-30 06:26:44 +0200

    initial commit, version 0.9

---
+++ javascript/code.js
@@ -0,0 +1,35 @@
+var data1 = 'Hello world!'
+var data2 = ['First','Second','Third']
+var data3 = [
+  {'.name':'Ben','.name@href':'#/a','.age':24},
+  {'.name':'Bob','.name@href':'#/b','.age':25},
+  {'.name':'Brad','.name@href':'#/c','.age':26}
+]
+var data4 = [
+  {'@label':'First','option':[
+    {'':'A','@value':'1'},
+    {'':'B','@value':'2'}
+  ]},
+  {'@label':'Second','option':[
+    {'':'C','@value':'3'},
+    {'':'D','@value':'4'}
+  ]}
+]
+var data5 = [
+  {group:'Third',list:[{name:'E',value:5},{name:'F',value:6}]},
+  {group:'Forth',list:[{name:'G',value:7},{name:'H',value:8}]}
+]
+var map5 = {group:'@label',list:['option',{name:'',value:'@value'}]}
+
+$(function(){
+  $('#hello').render(data1);
+  $('#users').render(data2);
+  $('#users2').render(data3);
+  $('#users2 span.age').bind('click',function(e){
+    e.preventDefault();
+    alert($(this).html());
+    return false;
+  })
+  $('#opts>optgroup').render(data4);
+  $('#server_opts>optgroup').render(data5,map5);
+});
