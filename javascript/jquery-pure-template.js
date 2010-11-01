3d651e4631249901c28a3ec6e834debe3907d2e8
mpapis <mpapis@gmail.com>
2010-11-01 20:36:39 +0100

    fixed performance, refiling, multiple lists at once

--- javascript/jquery-pure-template.js
+++ javascript/jquery-pure-template.js
@@ -1,5 +1,5 @@
 /*
- * jQuery Pure Template plugin v 0.9.01
+ * jQuery Pure Template plugin v 0.9.02
  * Copyright 2010, Michal Papis <mpapis@niczsoft.com>
  * Licensed under the MIT or GPL Version 2 licenses.
  * http://jquery.org/license
@@ -7,16 +7,37 @@
 
 (function($){
   $.fn.render = function(data,map){
-    $_pt.logO('rendering',this.selector)
-    var current = this;
-    var selector = current.selector;
-    var template = current.data('_tp_template') || current;
-    var target = template.clone();
-    target.data('_tp_template',template);
-    var old = current.replaceWith(target);
-    old.remove();
-    target.selector = selector;
+    $_pt.logO('rendering',this.selector);
+    var target = this;
+    var templates = this.map(function(){
+      $_pt.logO('r:',this,$.data(this, '_tp_template'));
+      return $.data(this, '_tp_template') ? this : null;
+    });
+    if (templates.length>0) {
+      target = this.map(function(){
+        var elem = $(this);
+        var template = $.data(this,'_tp_template')
+        $_pt.logO('elem',elem,'template',template);
+        if (template) {
+          elem = $(this).replaceWith(template);
+          elem.remove();
+          return template;
+        } else {
+          elem.remove();
+          return null;
+        }
+      });
+      target.selector = this.selector;
+    }
+    target.each(function(){
+      $.data(this,'_tp_template',$(this).clone()[0]);
+      $_pt.logO('w:',this,$.data(this, '_tp_template'));
+    });
+    $_pt.logO('this',this,'temps',templates,'target',target);
     $_pt.render(target,data,map);
+    target.each(function(){
+      $_pt.logO('r2:',this,$.data(this, '_tp_template'));
+    });
   }
 })(jQuery)
 
@@ -31,27 +52,37 @@ $_pt.logO = function(){
 }
 
 $_pt.uniq_id = 1;
-$_pt.uniq = function() { return '_pt_key'+$_pt.uniq_id++ + '_'; }
+$_pt.uniq = function() {return '_pt_key'+$_pt.uniq_id++ + '_';}
 
 //fill element with data
 $_pt.render = function(root,data,map,attr) {
-  if (attr) {
-    $_pt.logO('root:',root,'map:',map,'selector:',root.selector,'data:',data,'attr:',attr);
-  } else {
-    $_pt.logO('root:',root,'map:',map,'selector:',root.selector,'data:',data);
-  }
+  var t1 = new Date().getTime();
+  $_pt.logO('root:',root,'map:',map,'selector:',root.selector,'data:',data,'attr:',attr);
   var newElem;
   if (attr){
     root.attr(attr,data);
   } else if (data instanceof Array) {
+    var uniq = $_pt.uniq();
+    var first = null;
     for (var id in data){
       newElem = root.clone();
-      var uniq = $_pt.uniq();
-      newElem.addClass(uniq+id);
-      newElem.selector=root.selector+'.'+uniq+id;
+      if (!first) first = newElem;
+      if (data[id]['id']) {
+        var obj_id = data[id]['id'];
+        delete data[id]['id'];
+        newElem.attr('id',obj_id)
+        newElem.selector=root.selector+'#'+obj_id;
+      } else {
+        newElem.addClass(uniq+id);
+        newElem.selector=root.selector+'.'+uniq+id+':first';
+      }
       root.before(newElem);
       $_pt.render(newElem,data[id],map);
     }
+    first.each(function(id){
+      var template = root[id] ? $.data(root[id],'_tp_template') : undefined;
+      if (template) $.data(first[id],'_tp_template',template);
+    });
     root.remove();
   } else if (data instanceof String || typeof(data) === 'string') {
     root.html(data);
@@ -70,8 +101,10 @@ $_pt.render = function(root,data,map,attr) {
       var arr = key.split('@');
       attr = arr[1];
       key = arr[0];
-      newElem = ($.trim(key)==='') ? root : $(root.selector+' '+key);
+      newElem = ($.trim(key)==='') ? root : root.find(key);
       $_pt.render(newElem, val, map, attr);
     }
   }
+  var t2 = new Date().getTime();
+  $_pt.logO(''+(t2-t1)+'ms',root.selector,data);
 };
